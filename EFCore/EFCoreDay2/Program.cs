c0274b8369f1900b1a1a6b5454a9db6c682f6ea8
YousefAbdElrahman17 <jolmera2003@gmail.com>
2026-09-06 15:35:47 +0300

    Adding EFCore Day2 adding from existing DB

---
+++ EFCore/EFCoreDay2/Program.cs
@@ -0,0 +1,12 @@
+﻿/*
+cd "/mnt/windows/diff/projects/iti DotNet/EFCore/EFCoreDay2_2"
+install packages (sqlserver, tools, design)
+efcpt "Server=localhost,{}; Database=Airline; user id={}; Password={}; TrustServerCertificate=True" mssql 
+edit the .json file as u want .. edit tables by {"name": "[dbo].[Employee_Qualification]", "exclude":true} or
+                exclude all then false it.
+"file-layout": {"output-dbcontext-path": "Context", "output-path": "Models"},
+every edit use efcpt.... again
+*/
+
+
+// Code first from exist DB
