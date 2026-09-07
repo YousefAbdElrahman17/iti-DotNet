93a2b86a4cf251b5618d1a337748c8d0ad300563
YousefAbdElrahman17 <jolmera2003@gmail.com>
2026-09-07 15:32:57 +0300

    Adding MVC Day1 project, db.cs not added as usual

---
+++ MVC/MVCDay1/Controllers/EmployeeController.cs
@@ -0,0 +1,39 @@
+﻿﻿using Microsoft.AspNetCore.Mvc;
+using MVCDay1.Context;
+using MVCDay1.Models;
+
+namespace MVCDay1.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        CompanyDbContext db;
+        public EmployeeController()
+        {
+            db = new CompanyDbContext();
+        }
+        public IActionResult GetAll()
+        {
+            var emps = db.Employees.ToList();
+            return Json(emps);
+        }
+        [HttpGet]
+        public void add(Employee emp)
+        {
+            db.Add(emp);
+            db.SaveChanges();
+        }
+        [HttpGet]
+        public void update(Employee e)
+        {
+            db.Update(e);
+            db.SaveChanges();
+        }
+        [HttpGet]
+        public void delete(int id)
+        { 
+            var emp = db.Employees.SingleOrDefault(e => e.Id == id);
+            db.Employees.Remove(emp);
+            db.SaveChanges();
+        }
+    }
+}
