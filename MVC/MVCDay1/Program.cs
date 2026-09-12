64a8dcb430c3850a1ba3b86923f8c4591158f024
YousefAbdElrahman17 <jolmera2003@gmail.com>
2026-09-12 20:21:24 +0300

    Add Summary of  Creating and Dealing with the Project

--- MVC/MVCDay1/Program.cs
+++ MVC/MVCDay1/Program.cs
@@ -2,6 +2,32 @@ namespace MVCDay1
 {
     public class Program
     {
+        
+        /*
+        MVC (model view controller)
+         - Model: has db, classes..etc
+         - View: ui html
+         - controller: receive requests when an action happens
+        data seeding insert data in db context
+        .Net, Asp.net Core web app (MVC) not ConsoleApp or Razor
+        cd/project
+        Add packages (EFCore)
+        Make sure of new db name
+        dotnet build
+        dotnet ef migrations add migName
+        dotnet ef database update
+        dotnet run, cancle by ctrl+c
+        close all run by (pkill -9 -f dotnet)
+        (dotnet watch run: Auto Reload & Rebuild) after edits
+        After Run, take Port code from terminal
+        Now listening on: https://localhost:7XXX
+        Now listening on: http://localhost:5076
+        https://localhost:{PORT}/Employee/GetAll
+        https://localhost:PORT/Employee/add?Name=Osama&Age=27&Salary=5200&DeptId=1
+        https://localhost:PORT/Employee/update?Id=1&Name=JohnUpdated&Age=31&Salary=52000&DeptId=2
+        https://localhost:PORT/Employee/delete?id=1    
+        */
+
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
